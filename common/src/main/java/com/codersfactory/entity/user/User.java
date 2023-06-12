53b250640eab8969c3ac5b49e8dbdf9dd376afc5
aleksandrawlazlo <aleksandra.wlazlo97@gmail.com>
2023-06-12 12:18:22 +0200

    Created user entity.

--- common/src/main/java/com/codersfactory/entity/user/User.java
+++ common/src/main/java/com/codersfactory/entity/user/User.java
@@ -1,2 +1,36 @@
-package com.codersfactory.entity.user;public class User {
+package com.codersfactory.entity.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name="user_id")
+    private Long userId;
+
+    private String username;
+
+    private String email;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="total_points")
+    private int totalPoints;
+
 }
