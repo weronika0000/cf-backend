beda3728eaae5a27653aa7f84582f33f9e4f71fd
sylvvester <128241313+sylvvester@users.noreply.github.com>
2023-06-17 17:28:41 +0200

    Implement feature: Exception handling and error response for task-related operations

---
+++ task/src/main/java/com/codersfactory/control/exceptions/ErrorResponse.java
@@ -0,0 +1,36 @@
+package com.codersfactory.control.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class ErrorResponse {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private Date timestamp;
+
+    private int code;
+
+    private String status;
+
+    private String message;
+
+    private Object data;
+
+    private String stackTrace;
+
+    public ErrorResponse() {
+        this.timestamp = new Date();
+    }
+
+    public ErrorResponse(String status, String message) {
+        this.timestamp = new Date();
+        this.status = status;
+        this.message = message;
+    }
+
+}
