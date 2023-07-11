1136146a73b911e10937be1a398f79bc1235d897
aleksandrawlazlo <aleksandra.wlazlo97@gmail.com>
2023-07-11 09:38:29 +0200

    TaskSolution entity created

---
+++ src/main/java/com/codersfactory/task_solution/TaskSolution.java
@@ -0,0 +1,44 @@
+package com.codersfactory.task_solution;
+
+import com.codersfactory.task.Task;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class TaskSolution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    private Long userId;
+
+    private String userSolution;
+
+    private int pointsReceived;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+    private boolean isFinished;
+
+}
