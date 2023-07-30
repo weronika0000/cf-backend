e129027dd139d94b6744b5eef55224855df34e43
Weronika <133650676+weronika0000@users.noreply.github.com>
2023-07-30 04:46:13 +0200

    Feature/task service tests (#62)
    * changes in task service tests
    
    * changes in task service test
    
    * chagnes in task service tests
    
    * changes in task service tests
    
    * changes in task service tests
    
    * changes in task service test
    
    * changes in task service tests

--- src/test/java/com/codersfactory/task/TaskServiceImplTest.java
+++ src/test/java/com/codersfactory/task/TaskServiceImplTest.java
@@ -1,160 +1,191 @@
 package com.codersfactory.task;
 
+import com.codersfactory.article.Article;
 import com.codersfactory.common.entity.DifficultyLevel;
 import com.codersfactory.task.exception.TaskNotFoundException;
 import com.codersfactory.common.exception.UserNotAuthorizedException;
 import com.codersfactory.task.dto.CreateTaskRequestDto;
 import com.codersfactory.task.dto.CreateTaskResponseDto;
 import com.codersfactory.task.dto.TaskResponseDto;
+import com.codersfactory.task_solution.TaskSolution;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Optional;
+import java.util.Set;
 
+import static com.codersfactory.common.entity.DifficultyLevel.INTERMEDIATE;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TaskServiceImplTest {
+    CreateTaskRequestDto CreateTaskRequestDto = new CreateTaskRequestDto("Test Title", "Test Content", "Test Example Solution", "Test Hint", 5, INTERMEDIATE, 1L, "Test technology", "Test tests");
+    private static Long TASK_ID = 1L;
+    private static final String TITLE= "Test Title";
+    private static final String UPDATED_TITLE = "Updated Title";
+    private static final String CONTENT  = "Test Content";
+    private static final String EXAMPLE_SOLUTION = "Test Example Solution";
+    private static final String HINT = "Test Hint";
+    private static final int NUMBER_OF_POINTS = 5;
+    private static final DifficultyLevel DIFFICULTY_LEVEL = INTERMEDIATE;
+    private static final Long CREATOR_ID = 1L;
+    private static final Instant CREATED_AT= Instant.now();
+    private static final Instant UPDATED_AT= Instant.now();
+    private static final  Duration AVERAGE_COMPLETION_TIME = null;
+    private static final String TECHNOLOGY ="Test technology";
+    private static final String TESTS = "Test tests";
+    private static final boolean IF_APPROVED = false;
+    private static final Set<TaskSolution> TASK_SOLUTION = null;
+    private static final Article ARTICLE_ID = null;
+
+    private static final CreateTaskRequestDto CREATE_TASK_REQUEST_DTO =
+            new CreateTaskRequestDto(TITLE, CONTENT, EXAMPLE_SOLUTION, HINT, NUMBER_OF_POINTS, DIFFICULTY_LEVEL, CREATOR_ID, TECHNOLOGY, TESTS);
+    private static final CreateTaskResponseDto CREATE_TASK_RESPONSE_DTO =
+            new CreateTaskResponseDto(CREATOR_ID, TITLE, CONTENT, EXAMPLE_SOLUTION, HINT, NUMBER_OF_POINTS, DIFFICULTY_LEVEL, CREATOR_ID, CREATED_AT, TECHNOLOGY, TESTS);
+    private static final TaskResponseDto TASK_RESPONSE_DTO =
+            new TaskResponseDto(CREATOR_ID, TITLE, CONTENT, EXAMPLE_SOLUTION, HINT, NUMBER_OF_POINTS, DIFFICULTY_LEVEL, CREATOR_ID, CREATED_AT, UPDATED_AT, AVERAGE_COMPLETION_TIME, TESTS, TESTS, TASK_SOLUTION, ARTICLE_ID);
+    private static final CreateTaskRequestDto UPDATED_CREATE_TASK_REQUEST_DTO =
+            new CreateTaskRequestDto(UPDATED_TITLE, CONTENT, EXAMPLE_SOLUTION, HINT, NUMBER_OF_POINTS, DIFFICULTY_LEVEL, CREATOR_ID, TECHNOLOGY, TESTS);
+    private TaskResponseDto UPDATED_TASK_RESPONSE_DTO =
+            new TaskResponseDto(CREATOR_ID, UPDATED_TITLE, CONTENT, EXAMPLE_SOLUTION, HINT, NUMBER_OF_POINTS, DIFFICULTY_LEVEL, CREATOR_ID, CREATED_AT, UPDATED_AT, AVERAGE_COMPLETION_TIME, TESTS, TESTS, TASK_SOLUTION, ARTICLE_ID);
 
     @Mock
     TaskRepository taskRepository;
-
     @Mock
     TaskMapper taskMapper;
-
     @InjectMocks
     private TaskServiceImpl taskService;
 
+    @DisplayName("test createTask: when parameters are valid should return taskResponseDto")
     @Test
     void testCreateTask_whenValidParametersProvided_thenReturnCreateTaskResponseDto() {
-        //Arrange
-        CreateTaskRequestDto CreateTaskRequestDto = new CreateTaskRequestDto("Test Title", "Test Content", "Test Example Solution", "Test Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, "Test technology", "Test tests");
-        Task taskFromRequest = new Task(null, "Test Title", "Test Content", "Test Example Solution", "Test Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, Instant.now(), Instant.now(), null, "Test technology", "Test tests", false, null, null);
-        Task taskFromDatabase = new Task(1L, "Test Title", "Test Content", "Test Example Solution", "Test Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, Instant.now(), Instant.now(), null, "Test technology", "Test tests", false, null, null);
-        CreateTaskResponseDto taskResponseDto = new CreateTaskResponseDto(1L, "Test Title", "Test Content", "Test Example Solution", "Test Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, Instant.now(), "Test technology", "Test tests");
+        //given
+        Task taskFromRequest = createTestTask();
+        Task taskFromDatabase = createTestTask();
+        taskFromDatabase.setTaskId(TASK_ID);
 
-        when(taskMapper.createTaskFromRequest(CreateTaskRequestDto)).thenReturn(taskFromRequest);
-        when(taskRepository.save(taskFromRequest)).thenReturn(taskFromDatabase);
-        when(taskMapper.createResponseDtoFromTask(taskFromDatabase)).thenReturn(taskResponseDto);
+        given(taskMapper.createTaskFromRequest(CREATE_TASK_REQUEST_DTO)).willReturn(taskFromRequest);
+        given(taskRepository.save(taskFromRequest)).willReturn(taskFromDatabase);
+        given(taskMapper.createResponseDtoFromTask(taskFromDatabase)).willReturn(CREATE_TASK_RESPONSE_DTO);
 
-        //Act
-        CreateTaskResponseDto result = taskService.createTask(CreateTaskRequestDto);
+        //when
+        CreateTaskResponseDto result = taskService.createTask(CREATE_TASK_REQUEST_DTO);
 
-        //Assert
-        assertEquals(taskResponseDto, result);
+        //then
+        assertEquals(CREATE_TASK_RESPONSE_DTO, result);
     }
 
+    @DisplayName("test getById: when task exists, then return taskResponseDto")
     @Test
     void testGetById_WhenTaskExists_ThenReturnTaskResponseDto() {
-        // Arrange
-        Long taskId = 1L;
-        Task taskFromDatabase = new Task(taskId, "Test Title", "Test Content", "Test Example Solution", "Test Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, Instant.now(), Instant.now(), null, "Test technology", "Test tests", false, null, null);
-        TaskResponseDto taskResponseDto = new TaskResponseDto(taskId, "Test Title", "Test Content", "Test Example Solution", "Test Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, Instant.now(), Instant.now(), null, "Test technology", "Test tests", null, null);
+        // given
+        Task taskFromDatabase = createTestTask();
+        taskFromDatabase.setTaskId(TASK_ID);
 
-        when(taskRepository.findById(taskId)).thenReturn(Optional.of(taskFromDatabase));
-        when(taskMapper.responseDtoFromTask(taskFromDatabase)).thenReturn(taskResponseDto);
+        given(taskRepository.findById(TASK_ID)).willReturn(Optional.of(taskFromDatabase));
+        given(taskMapper.responseDtoFromTask(taskFromDatabase)).willReturn(TASK_RESPONSE_DTO);
 
-        // Act
-        TaskResponseDto result = taskService.getById(taskId);
+        // when
+        TaskResponseDto result = taskService.getById(TASK_ID);
 
-        // Assert
-        assertEquals(taskResponseDto, result);
+        // then
+        assertEquals(TASK_RESPONSE_DTO, result);
     }
 
+    @DisplayName("test getById: when task exists, then throw TaskNotFoundException")
     @Test
     void testGetById_WhenTaskDoesNotExist_ThenThrowException() {
-        // Arrange
-        Long taskId = 1L;
-
-        when(taskRepository.findById(taskId)).thenReturn(Optional.empty());
-
-        // Act & Assert
-        assertThrows(TaskNotFoundException.class, () -> taskService.getById(taskId));
+        // given
+        given(taskRepository.findById(TASK_ID)).willReturn(Optional.empty());
+        // when & then
+        assertThrows(TaskNotFoundException.class, () -> taskService.getById(TASK_ID));
     }
 
+    @DisplayName("test updateTask: when valid parameters provided, then return updated taskResponseDto")
     @Test
     void testUpdateTask_WhenValidParametersProvided_ThenReturnTaskResponseDto() {
-        // Arrange
-        Long taskId = 1L;
-        CreateTaskRequestDto taskDto = new CreateTaskRequestDto("Updated Title", "Updated Content", "Updated Example Solution", "Updated Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, "Updated technology", "Updated tests");
-        Task taskFromDatabase = new Task(taskId, "Original Title", "Original Content", "Original Example Solution", "Original Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, Instant.now(), Instant.now(), null, "Original technology", "Original tests", false, null, null);
-        TaskResponseDto expectedResponseDto = new TaskResponseDto(taskId, "Updated Title", "Updated Content", "Updated Example Solution", "Updated Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, Instant.now(), Instant.now(), null, "Updated technology", "Updated tests", null, null);
+        // given
+        Task taskFromDatabase = createTestTask();
+        given(taskRepository.findById(TASK_ID)).willReturn(Optional.of(taskFromDatabase));
+        taskFromDatabase.setTitle(UPDATED_CREATE_TASK_REQUEST_DTO.title());
+        taskFromDatabase.setUpdatedAt(UPDATED_AT);
+        given(taskRepository.save(taskFromDatabase)).willReturn(taskFromDatabase);
+        given(taskMapper.responseDtoFromTask(taskFromDatabase)).willReturn(UPDATED_TASK_RESPONSE_DTO);
 
-        when(taskRepository.findById(taskId)).thenReturn(Optional.of(taskFromDatabase));
-        taskFromDatabase.setTitle(taskDto.title());
-        taskFromDatabase.setContent(taskDto.content());
-        taskFromDatabase.setExampleSolution(taskDto.exampleSolution());
-        taskFromDatabase.setHint(taskDto.hint());
-        taskFromDatabase.setNumberOfPoints(taskDto.numberOfPoints());
-        taskFromDatabase.setDifficultyLevel(taskDto.difficultyLevel());
-        taskFromDatabase.setTechnology(taskDto.technology());
-        taskFromDatabase.setTests(taskDto.tests());
-        taskFromDatabase.setUpdatedAt(Instant.now());
-        when(taskRepository.save(taskFromDatabase)).thenReturn(taskFromDatabase);
-        when(taskMapper.responseDtoFromTask(taskFromDatabase)).thenReturn(expectedResponseDto);
+        // when
+        TaskResponseDto result = taskService.updateTask(TASK_ID, UPDATED_CREATE_TASK_REQUEST_DTO);
 
-        // Act
-        TaskResponseDto result = taskService.updateTask(taskId, taskDto);
-
-        // Assert
-        assertEquals(expectedResponseDto, result);
+        // then
+        assertEquals(UPDATED_TASK_RESPONSE_DTO, result);
     }
-
+    @DisplayName("test updateTask: when task does not exist, then throw TaskNotFoundException")
     @Test
     void testUpdateTask_WhenTaskDoesNotExist_ThenThrowException() {
-        // Arrange
-        Long taskId = 1L;
-        CreateTaskRequestDto taskDto = new CreateTaskRequestDto("Updated Title", "Updated Content", "Updated Example Solution", "Updated Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, "Updated technology", "Updated tests");
-        when(taskRepository.findById(taskId)).thenReturn(Optional.empty());
+        // given
+        given(taskRepository.findById(TASK_ID)).willReturn(Optional.empty());
 
-        // Act & Assert
-        assertThrows(TaskNotFoundException.class, () -> taskService.updateTask(taskId, taskDto));
+        // when & then
+        assertThrows(TaskNotFoundException.class, () -> taskService.updateTask(TASK_ID, UPDATED_CREATE_TASK_REQUEST_DTO));
     }
-
+    @DisplayName("test updateTask: when user not authorized, then throw UserNotAuthorizedException")
     @Test
     void testUpdateTask_WhenUserNotAuthorized_ThenThrowException() {
-        // Arrange
-        Long taskId = 1L;
-        CreateTaskRequestDto taskDto = new CreateTaskRequestDto("Updated Title", "Updated Content", "Updated Example Solution", "Updated Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, "Updated technology", "Updated tests");
-        Task taskFromDatabase = new Task(taskId, "Original Title", "Original Content", "Original Example Solution", "Original Hint", 5, DifficultyLevel.INTERMEDIATE, 2L, Instant.now(), Instant.now(), null, "Original technology", "Original tests", false, null, null);
+        // given
+        Task taskFromDatabase = createTestTask();
+        taskFromDatabase.setCreatorId(2L);
 
-        when(taskRepository.findById(taskId)).thenReturn(Optional.of(taskFromDatabase));
+        given(taskRepository.findById(TASK_ID)).willReturn(Optional.of(taskFromDatabase));
 
-        // Act & Assert
-        assertThrows(UserNotAuthorizedException.class, () -> taskService.updateTask(taskId, taskDto));
+        // when & Assert
+        assertThrows(UserNotAuthorizedException.class, () -> taskService.updateTask(TASK_ID, UPDATED_CREATE_TASK_REQUEST_DTO));
     }
-
+    @DisplayName("test deleteTaskById: when valid parameters provided, then delete task")
     @Test
     void testDeleteTaskById_whenValidParametersProvided() {
-        // Arrange
-        Long taskId = 1L;
-        CreateTaskRequestDto taskDto = new CreateTaskRequestDto("Updated Title", "Updated Content", "Updated Example Solution", "Updated Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, "Updated technology", "Updated tests");
-        Task taskFromDatabase = new Task(taskId, "Original Title", "Original Content", "Original Example Solution", "Original Hint", 5, DifficultyLevel.INTERMEDIATE, 1L, Instant.now(), Instant.now(), null, "Original technology", "Original tests", false, null, null);
+        // given
+        Task taskFromDatabase = createTestTask();
+        given(taskRepository.findById(TASK_ID)).willReturn(Optional.of(taskFromDatabase));
 
-        when(taskRepository.findById(taskId)).thenReturn(Optional.of(taskFromDatabase));
-
-        // Act
-        taskService.deleteTaskById(taskId);
-
-        // Assert
+        // when
+        taskService.deleteTaskById(TASK_ID);
+        // then
         verify(taskRepository, times(1)).delete(taskFromDatabase);
     }
-
+    @DisplayName("test deleteTaskById: when task not found, then throw TaskNotFoundException")
     @Test
     void testDeleteTaskById_whenTaskNotFound_ThenThrowException() {
-        // Arrange
-        Long taskId = 1L;
-
-        when(taskRepository.findById(taskId)).thenReturn(Optional.empty());
-
-        // Act & Assert
-        assertThrows(TaskNotFoundException.class, () -> taskService.deleteTaskById(taskId));
+        // given
+        given(taskRepository.findById(TASK_ID)).willReturn(Optional.empty());
+        // when & then
+        assertThrows(TaskNotFoundException.class, () -> taskService.deleteTaskById(TASK_ID));
     }
+    private Task createTestTask() {
+        return Task.builder()
+                .taskId(TASK_ID)
+                .title(TITLE)
+                .content(CONTENT)
+                .exampleSolution(EXAMPLE_SOLUTION)
+                .hint(HINT)
+                .numberOfPoints(NUMBER_OF_POINTS)
+                .difficultyLevel(INTERMEDIATE)
+                .creatorId(CREATOR_ID)
+                .createdAt(CREATED_AT)
+                .averageCompletionTime(AVERAGE_COMPLETION_TIME)
+                .technology(TECHNOLOGY)
+                .tests(TESTS)
+                .ifApproved(IF_APPROVED)
+                .build();
+
+    }
+
 }
 
 
